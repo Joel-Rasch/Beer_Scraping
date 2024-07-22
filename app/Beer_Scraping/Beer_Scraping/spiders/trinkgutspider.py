8ea46db9fde8cbafd18194bfe43d9c99e827dab8
JoeCodity <joel.rasch98@gmail.com>
2024-07-22 16:24:12 +0200

    Changes structure

--- app/Beer_Scraping/Beer_Scraping/spiders/trinkgutspider.py
+++ app/Beer_Scraping/Beer_Scraping/spiders/trinkgutspider.py
@@ -4,7 +4,10 @@ import scrapy
 class TrinkgutspiderSpider(scrapy.Spider):
     name = "trinkgutspider"
     allowed_domains = ["www.trinkgut.de"]
-    start_urls = ["https://www.trinkgut.de/search?search=bier"]
+    start_urls = ["https://www.trinkgut.de/search?order=score&p=1&search=bier"]
 
     def parse(self, response):
+        beers = response.xpath('//*[contains(@class,"product-box")]/div/a/@href').extract()
+        next_page = response.xpath('//*[contains(@id,"p-next-bottom")]/@value').extract()
+
         pass
